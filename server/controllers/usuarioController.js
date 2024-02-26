351116da94d1722eced1ac4b437022b52905edf9
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-02-26 10:11:18 -0300

    editar y elimiar usuario

--- server/controllers/usuarioController.js
+++ server/controllers/usuarioController.js
@@ -20,3 +20,24 @@ exports.obtenerUsuarios = async (req, res) => {
   }
 };
 
+// Actualizar usuario
+exports.actualizarUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(usuario);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+// Eliminar usuario
+exports.eliminarUsuario = async (req, res) => {
+  try {
+    await Usuario.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Usuario eliminado correctamente' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+
